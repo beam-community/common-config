fc2da0b5f0f3299738f595b2fc8e80839c51243a
Yordis Prieto <yordis.prieto@gmail.com>
2026-07-14 13:49:54 -0400

    fix: detect public package function head in mix.exs (#105)
    Signed-off-by: Yordis Prieto <yordis.prieto@gmail.com>

--- scripts/001.detect-elixir-package.sh
+++ scripts/001.detect-elixir-package.sh
@@ -3,7 +3,7 @@
 if [ -f "mix.exs" ]; then
   echo "mix.exs file detected"
 
-  if grep -q "defp package do" mix.exs; then
+  if grep -qE "defp? package do" mix.exs; then
     echo "Detected a package function head in mix.exs"
     echo "IS_MIX_PACKAGE=true" >> "$TEMPLATE_ENV"
 
